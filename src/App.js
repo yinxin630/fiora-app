ea9a18447fdd4fa43f7cde1f9063a9b37d37003b
碎碎酱 <yinxin630@gmail.com>
2018-05-31 00:15:30 +0800

    feat: Add the eslint configuration, add the socket.io-client package

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Hello, Fiora</Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
