d99915f87b6a49aca5d8f82504d7df7f2444ba00
碎碎酱 <yinxin630@gmail.com>
2018-06-08 22:08:54 +0800

    feat: Show more user device information

--- utils/platform.js
+++ utils/platform.js
@@ -1,14 +1,15 @@
 import { Platform } from 'react-native';
+import { Constants } from 'expo';
 import packageInfo from '../package';
 
 const os = Platform.OS === 'ios' ? 'iOS' : 'Android';
 
+export const isiOS = Platform.OS === 'ios';
+export const isAndroid = Platform.OS === 'android';
+
 export default {
     os,
     browser: 'App',
-    environment: `App ${process.env.NODE_ENV === 'development' ? '开发版' : packageInfo.version} on ${os} ${Platform.Version}`,
+    environment: `App ${process.env.NODE_ENV === 'development' ? '开发版' : packageInfo.version} on ${os} ${isiOS ? Constants.platform.ios.systemVersion : Constants.systemVersion} ${isiOS ? Constants.platform.ios.model : ''}`,
 };
-
-export const isiOS = Platform.OS === 'ios';
-export const isAndroid = Platform.OS === 'android';
 
