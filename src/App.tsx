8f745d5638ddfd012e2e85747a8d2fae3993b1ce
碎碎酱 <yinxin630@gmail.com>
2021-02-25 22:17:29 +0800

    Handle notification click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { AppState, Platform, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { Scene, Router, Stack, Tabs, Lightbox } from 'react-native-router-flux';
 import { Icon, Root } from 'native-base';
-import * as Notifications from 'expo-notifications';
 
 import { connect } from 'react-redux';
-import Constants from 'expo-constants';
 import ChatList from './pages/ChatList/ChatList';
 import Chat from './pages/Chat/Chat';
 import Login from './pages/LoginSignup/Login';
@@ -14,97 +12,17 @@ import Test from './pages/test';
 
 import Loading from './components/Loading';
 import Other from './pages/Other/Other';
+import Notification from './components/Nofitication';
 import { State, User } from './types/redux';
 import SelfInfo from './pages/ChatList/SelfInfo';
-import { setNotificationToken } from './service';
-import { isiOS } from './utils/platform';
-
-Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-        shouldShowAlert: false,
-        shouldPlaySound: false,
-        shouldSetBadge: false,
-    }),
-});
 
 type Props = {
     title: string;
     primaryColor: string;
-    notificationTokens: string[];
     isLogin: boolean;
-    isConnect: boolean;
 };
 
-function App({ title, primaryColor, notificationTokens, isLogin, isConnect }: Props) {
-    const [notificationToken, updateNotificationToken] = useState('');
-
-    async function registerForPushNotificationsAsync() {
-        // Push notification to Android device need google service
-        // Not supported in China
-        if (Constants.isDevice && isiOS) {
-            const { status: existingStatus } = await Notifications.getPermissionsAsync();
-            let finalStatus = existingStatus;
-            if (existingStatus !== 'granted') {
-                const { status } = await Notifications.requestPermissionsAsync();
-                finalStatus = status;
-            }
-            if (finalStatus !== 'granted') {
-                return;
-            }
-            const token = (await Notifications.getExpoPushTokenAsync()).data;
-            updateNotificationToken(token);
-
-            if (Platform.OS === 'android') {
-                Notifications.setNotificationChannelAsync('default', {
-                    name: 'default',
-                    importance: Notifications.AndroidImportance.MAX,
-                    vibrationPattern: [0, 250, 250, 250],
-                    lightColor: '#FF231F7C',
-                });
-            }
-        }
-    }
-    useEffect(() => {
-        registerForPushNotificationsAsync();
-    }, []);
-
-    useEffect(() => {
-        if (
-            isConnect &&
-            isLogin &&
-            notificationToken &&
-            !notificationTokens.includes(notificationToken)
-        ) {
-            setNotificationToken(notificationToken);
-        }
-    }, [isConnect, isLogin, notificationToken]);
-
-    function handleAppStateChange(nextAppState: string) {
-        if (nextAppState === 'active') {
-            Notifications.setNotificationHandler({
-                handleNotification: async () => ({
-                    shouldShowAlert: false,
-                    shouldPlaySound: false,
-                    shouldSetBadge: false,
-                }),
-            });
-        } else if (nextAppState === 'background') {
-            Notifications.setNotificationHandler({
-                handleNotification: async () => ({
-                    shouldShowAlert: true,
-                    shouldPlaySound: true,
-                    shouldSetBadge: false,
-                }),
-            });
-        }
-    }
-    useEffect(() => {
-        AppState.addEventListener('change', handleAppStateChange);
-        return () => {
-            AppState.removeEventListener('change', handleAppStateChange);
-        };
-    }, []);
-
+function App({ title, primaryColor, isLogin }: Props) {
     const primaryColor10 = `rgba(${primaryColor}, 1)`;
     const primaryColor8 = `rgba(${primaryColor}, 0.8)`;
 
@@ -200,15 +118,14 @@ function App({ title, primaryColor, notificationTokens, isLogin, isConnect }: Pr
             </Root>
 
             <Loading />
+            <Notification />
         </View>
     );
 }
 
 export default connect((state: State) => ({
     primaryColor: state.ui.primaryColor,
-    notificationTokens: (state.user as User)?.notificationTokens || [],
     isLogin: !!(state.user as User)?._id,
-    isConnect: state.connect,
 }))(App);
 
 const styles = StyleSheet.create({
