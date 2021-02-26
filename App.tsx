9ac89acf874d7fbe24789f2e7e6f11d99785ebd9
碎碎酱 <yinxin630@gmail.com>
2021-02-26 21:21:57 +0800

    Use ActionSheet instead of popup menu

--- App.tsx
+++ App.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 import { Provider } from 'react-redux';
-import { MenuProvider } from 'react-native-popup-menu';
 import App from './src/App';
 import store from './src/state/store';
 
 export default function Main(props: any) {
     return (
         <Provider store={store}>
-            <MenuProvider>
-                <App {...props} />
-            </MenuProvider>
+            <App {...props} />
         </Provider>
     );
 };
